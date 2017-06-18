19e2567e22e31507ddca4987838b3559fb535c4f
ikenus <ikenus@gmail.com>
2017-06-18 19:36:24 +0200

    Setup iniziale

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react'
+
+class Home extends React.Component {
+  render () {
+    return(
+      <ul>
+        <li>Home</li>
+        <li>List</li>
+        <li>List2</li>
+        <li>DOM</li>
+      </ul>
+    )
+  }
+}
+
+export default Home ;
